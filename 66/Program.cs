0f3eb3553856c4b6a805edb50296407e497bf8ed
Kolesnik Anton <kolesnikas2006@gmail.com>
2022-12-10 21:15:54 +0500

    first commit

---
+++ 66/Program.cs
@@ -0,0 +1,16 @@
+﻿int m = 5;
+int n = 8;
+int CalculateSumm(int m, int n)
+	{
+		int start = m;
+		int end = n;
+		if(m > n)
+		{
+			start = n;
+			end = m;
+		}
+		return (end + start)*(end - start + 1)/2;
+	}
+		
+	Console.WriteLine(CalculateSumm(72, 16));
+	
